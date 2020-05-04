0752c76874cee05e1d3bea69f005cd720d8a81cd
mpara0 <monicapara2019@gmail.com>
2020-05-03 23:27:31 -0500

    Added UI to About Page

--- app/src/main/java/com/example/recycleit/About.java
+++ app/src/main/java/com/example/recycleit/About.java
@@ -2,7 +2,9 @@ package com.example.recycleit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        //button to return home
+        findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(About.this, MainActivity.class);
+                About.this.startActivity(intent);
+            }
+        });
     }
 }
