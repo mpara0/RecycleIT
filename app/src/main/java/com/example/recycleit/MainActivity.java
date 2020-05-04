bc8bf905e5a07cede7e1d6799ab46a3c107c769e
jguerrero836 <51328403+jguerrero836@users.noreply.github.com>
2020-05-03 19:10:32 -0500

    all pages created, algorithm for GET is commented out for now

--- app/src/main/java/com/example/recycleit/MainActivity.java
+++ app/src/main/java/com/example/recycleit/MainActivity.java
@@ -1,8 +1,14 @@
+/**
+ * main page that the user will open up and direct him to the rest of the pages
+ */
 package com.example.recycleit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //button for resources page
+        findViewById(R.id.resources).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Resources.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        //button for about page
+        findViewById(R.id.about).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, About.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        //button for search page
+        findViewById(R.id.startRecycling).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Search.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/recycleit/Resources.java
+++ app/src/main/java/com/example/recycleit/Resources.java
@@ -1,8 +1,13 @@
+/**
+ * this page will contain information on recycling(what can and can't be recycled, etc)
+ */
 package com.example.recycleit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Resources extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class Resources extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+        //button to return home
+        findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Resources.this, MainActivity.class);
+                Resources.this.startActivity(intent);
+            }
+        });
     }
 }
